25191af37bf39b08cafb5b8d431affff3f21952e
venton <pelebrufer56@gmail.com>
2025-11-01 14:32:23 -0300

    terceira atualização para corrigir violação google

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
 // next.config.js
-
 const securityHeaders = [
   {
     key: "Content-Security-Policy",
     value: `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com;
-      connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com;
-      img-src 'self' data: blob: https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com;
-      frame-src 'self' https://www.googletagmanager.com https://www.youtube.com https://www.google.com;
-      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-      font-src 'self' https://fonts.gstatic.com;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com https://connect.facebook.net;
+      connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://analytics.google.com https://connect.facebook.net;
+      img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://connect.facebook.net;
+      style-src 'self' 'unsafe-inline';
+      frame-src https://www.googletagmanager.com https://www.google.com https://connect.facebook.net;
     `.replace(/\s{2,}/g, ' ').trim(),
   },
 ];
@@ -25,5 +23,6 @@ module.exports = {
     ];
   },
 };
+
 
 
