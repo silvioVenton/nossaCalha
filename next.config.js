8687924d001d87b3dd0c7868d4a94357b5bc4549
venton <pelebrufer56@gmail.com>
2025-11-01 14:28:56 -0300

    nova atualização corrigindo violação do google tag manager

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const securityHeaders = [
     key: "Content-Security-Policy",
     value: `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://www.googletagmanager.com/gtag/js;
-      connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com;
-      img-src 'self' data: blob: https://www.google-analytics.com https://www.googletagmanager.com;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com;
+      connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com;
+      img-src 'self' data: blob: https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com;
       frame-src 'self' https://www.googletagmanager.com https://www.youtube.com https://www.google.com;
       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
       font-src 'self' https://fonts.gstatic.com;
@@ -25,4 +25,5 @@ module.exports = {
     ];
   },
 };
+
 
