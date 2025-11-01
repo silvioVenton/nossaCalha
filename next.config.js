01d6fb613f059fea79920ed037fd972d2a4ca148
venton <pelebrufer56@gmail.com>
2025-11-01 14:36:17 -0300

    nova

--- next.config.js
+++ next.config.js
@@ -1,15 +1,37 @@
 // next.config.js
+
 const securityHeaders = [
   {
     key: "Content-Security-Policy",
     value: `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com https://connect.facebook.net;
-      connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://analytics.google.com https://connect.facebook.net;
-      img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://connect.facebook.net;
-      style-src 'self' 'unsafe-inline';
-      frame-src https://www.googletagmanager.com https://www.google.com https://connect.facebook.net;
-    `.replace(/\s{2,}/g, ' ').trim(),
+      script-src 'self' 'unsafe-inline' 'unsafe-eval'
+        https://www.googletagmanager.com
+        https://www.google-analytics.com
+        https://www.google.com
+        https://connect.facebook.net
+        https://stats.g.doubleclick.net;
+      connect-src 'self'
+        https://www.google-analytics.com
+        https://www.googletagmanager.com
+        https://www.google.com
+        https://analytics.google.com
+        https://connect.facebook.net
+        https://stats.g.doubleclick.net;
+      img-src 'self' data: blob:
+        https://www.google-analytics.com
+        https://www.googletagmanager.com
+        https://www.google.com
+        https://connect.facebook.net
+        https://stats.g.doubleclick.net;
+      style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+      font-src 'self' https://fonts.gstatic.com;
+      frame-src 'self'
+        https://www.googletagmanager.com
+        https://www.google.com
+        https://connect.facebook.net
+        https://www.youtube.com;
+    `.replace(/\s{2,}/g, " ").trim(),
   },
 ];
 
@@ -23,6 +45,7 @@ module.exports = {
     ];
   },
 };
+
 
 
 
